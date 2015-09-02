5f7dc707f83e4bd3a2307c316f88a5a7c31fdb3e
Fysikerna <erian655@student.liu.se>
2015-09-02 20:52:29 +0200

    Kurs
    Lektionsuppgift 1.5
    Studenters betyg

---
+++ lesson_1_5.m
@@ -0,0 +1,6 @@
+disp('Nu körs skriptet!');
+student1 = 3; % Betyg som student 1 och 2 hade på en tenta
+student2 = 5;
+medel = (student1 + student2)/2; % Beräkna medelbetyget
+disp('Resultat:');
+display(medel);
