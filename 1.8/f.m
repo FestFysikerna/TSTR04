e23c40feeaa19cbf669030d0e51a016f031f61dc
Fysikerna <erian655@student.liu.se>
2015-09-02 22:57:29 +0200

    Kurs
    Skissa egen graf
    Moddad

--- 1.8/f.m
+++ 1.8/f.m
@@ -1,3 +1,5 @@
 function Y = f(x)
-Y(:,1) = (x^3)/(x^2-(2*abs(x-2)));
+Y(:,1) = (x^3)/(x^2-(2*abs(x-2))); %funktionen f(x)
+% Y(:,2) = x^2 % Det går att lägga till flera funktioner.
+% Y(:,3) = ....
 end
