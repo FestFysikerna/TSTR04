96451e343eff2a6c2c4a596835dc7a629b2e8eb5
Fysikerna <erian655@student.liu.se>
2015-09-02 21:52:58 +0200

    Kurs
    Skriva egna funktioner

---
+++ 1.7/medelbetyg.m
@@ -0,0 +1,8 @@
+function medel = medelbetyg(student1,student2)
+% Funktion som beräknar medelbetyg för två studenter.
+% Inargument: Betygen som student 1 och 2 hade på en tenta.
+% Utargument: Medelbetyget.
+disp('Nu körs funktionen!');
+medel = (student1 + student2)/2; % Här beräknas medelbetyget
+disp('Resultat:');
+display(medel);
