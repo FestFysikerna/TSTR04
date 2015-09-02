b2b16bf039b0b9e618d5bd9be892c65e4f0dbee2
Fysikerna <erian655@student.liu.se>
2015-09-02 23:26:49 +0200

    Kurs
    Lagt till kommentarer

--- 1.8/graph.m
+++ 1.8/graph.m
@@ -1,2 +1,2 @@
-fh = @f;
-fplot(fh,[-20 20]);
+fh = @f; %lägger till filen "f.m" i en variabel
+fplot(fh,[-20 20]); %Plottar upp funktionen i filen som finns i variabeln fh
--- 1.8/graph2.m
+++ 1.8/graph2.m
@@ -1,6 +1,5 @@
-fh = @f;
-figure
-fplot(fh,[-20 20])
-title('F(x)=(x^3)/(x^2-(2*abs(x-2))')
-xlabel('x-axel')
-ylabel('y-axel')
+fh = @f; %lägger till filen "f.m" i en variabel
+fplot(fh,[-20 20]); %Plottar upp funktionen i filen som finns i variabeln fh
+title('F(x)=(x^3)/(x^2-(2*abs(x-2))') %namnger titeln
+xlabel('x-axel') %namnger x-axeln
+ylabel('y-axel') %namnger y-axeln
