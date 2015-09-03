4c91cfb99880cb4a4765fe5181ca6c159b1689a0
Fysikerna <erian655@student.liu.se>
2015-09-03 14:11:44 +0200

    Kurs
    Lite allt möjligt

---
+++ 1.9/upp3.m
@@ -0,0 +1,6 @@
+format long %skriver ut talet med många decimaler
+if log(203.8)/sqrt(9*pi)>1 % om påståendet är sant
+    disp('yes'); %skriver ut i ren text
+else %om inte påståendet är sant
+    disp('no'); 
+end
